6acae7ac334e09745461a8b948c35cf9be424069
bruno <bruno.resende@gmx.com>
2023-06-29 16:11:07 -0300

    Feat: Added get_int

---
+++ src/bin/week_01_change.rs
@@ -0,0 +1,6 @@
+use cs50_rust::get_int;
+
+fn main() {
+    let bill = get_int("Please insert the bill");
+    println!("{bill}");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
+use std::io;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn get_int(message: &str) -> u32 {
+    println!("{message}");
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read the input.");
+
+        match input.trim().parse::<u32>() {
+            Ok(input) => return input,
+            Err(_) => {
+                println!("Invalid input. Please insert a number!");
+                continue;
+            }
+        };
+    }
 }
 
 #[cfg(test)]
